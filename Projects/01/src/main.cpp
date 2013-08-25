2105132b5f499fc1df5ed3dd261ed6daedb02c0d
Andrew DeMaria <ademaria@mines.edu>
2013-08-25 09:27:58 -0600

    Added simple main driver

---
+++ Projects/01/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ * Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+ *
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Ray Tracer" << endl;
+	cout << "  -- Andrew DeMaria" << endl;
+}
+
