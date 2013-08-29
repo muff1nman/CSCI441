aaf9ff6aa546796aa025dd269f176ac16dab6094
Andrew DeMaria <ademaria@mines.edu>
2013-08-28 23:21:34 -0600

    Added abstract class for finding intersections

---
+++ Projects/01/include/raytracer/intersection/intersectee.h
@@ -0,0 +1,23 @@
+/*
+ * intersectee.h
+ * Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+ *
+ * All Rights Reserved.
+ */
+
+#ifndef __intersectee_h__
+#define __intersectee_h__
+
+/**
+ * An abstract class for adding the capability for a shape to know if it is
+ * intersected.
+ */
+class Intersectee {
+
+	virtual bool is_intersected( Ray r );
+
+	virtual double intersected_at( Ray r );
+
+};
+#endif /* !__intersectee_h__ */
+
