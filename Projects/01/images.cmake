c9d693345d65cd1a4c4f7b54cbee9598517bda7d
Andrew DeMaria <ademaria@mines.edu>
2013-09-08 22:47:05 -0600

    Show stderr

--- Projects/01/images.cmake
+++ Projects/01/images.cmake
@@ -12,6 +12,5 @@ foreach(_d_file ${INPUT_FILES} )
 	get_filename_component(_file ${_d_file} NAME)
 	string(REGEX REPLACE "\\.txt" ".ppm" _image ${_file})
 	execute_process(
-		COMMAND ${SCRIPT_NAME} ${INPUT_FILE_DIR}/${_file} ${OUTPUT_IMAGE_DIR}/${_image}
-		ERROR_QUIET)
+		COMMAND ${SCRIPT_NAME} ${INPUT_FILE_DIR}/${_file} ${OUTPUT_IMAGE_DIR}/${_image})
 endforeach()
