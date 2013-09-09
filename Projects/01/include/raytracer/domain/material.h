ed6e5124f6a895ec24f73cddd0606e86091f7ead
Andrew DeMaria <ademaria@mines.edu>
2013-09-09 14:34:21 -0600

    Rearrange material code

--- Projects/01/include/raytracer/domain/material.h
+++ Projects/01/include/raytracer/domain/material.h
@@ -10,6 +10,7 @@
 
 #include "raytracer/domain/RGB.h"
 #include "raytracer/environment/light_source.h"
+#include "raytracer/domain/ray.h"
 
 #include <cmath>
 
@@ -18,33 +19,16 @@ class Material : public Logging {
 		Material(const RGB& k_diff, const RGB& k_ambient, double k_specular, double n_specular ) : 
 			k_diff(k_diff), k_ambient(k_ambient), k_specular(k_specular), n_specular(n_specular) { }
 
-		RGB illuminate(const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
-			if( in_shadow_at(light, view_ray, t_of_intersect) ) {
-				return illuminate_ambient(light);
-			} else {
-				return illuminate_diffused(light, view_ray, t_of_intersect) + illuminate_specular(light, view_ray, t_of_intersect) + illuminate_ambient(light);
-			}
-		}
+		RGB illuminate(const LightSource& light, const Ray& view_ray, double t_of_intersect) const;
 
-		Vector_3D light_vector_to_object( const LightSource& light, const Ray& view_ray, double t_of_intersect ) const {
-			return light.light_source_location - view_ray.at(t_of_intersect);
-		}
+		Vector_3D light_vector_to_object( const LightSource& light, const Ray& view_ray, double t_of_intersect ) const;
 
 		virtual Vector_3D normal_at(const Ray& view_ray, double t_of_intersect) const = 0;
 
-		bool in_shadow_at( const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
-			return light_vector_to_object( light, view_ray, t_of_intersect ) * normal_at(view_ray, t_of_intersect) < 0;
-		}
+		bool in_shadow_at( const LightSource& light, const Ray& view_ray, double t_of_intersect) const;
 
 #ifdef LOGGING
-		std::string stringify_object() const {
-			std::string info = "";
-			info += "k_diff: " + k_diff.to_string() + list_sep;
-			info += "k_ambient: " + k_ambient.to_string() + list_sep;
-			info += "k_specular: " + boost::lexical_cast<std::string>( k_specular ) + list_sep;
-			info += "n_specular: " + boost::lexical_cast<std::string>( n_specular ) + sep;
-			return info;
-		}
+		std::string stringify_object() const;
 #endif
 
 	protected:
@@ -54,25 +38,13 @@ class Material : public Logging {
     double n_specular;
 
 		// TODO put in util?
-		Vector_3D halfway_vector( const Vector_3D& viewpoint, const Vector_3D& light_source) const {
-			return (viewpoint + light_source) * 0.5;
-		}
+		Vector_3D halfway_vector( const Vector_3D& viewpoint, const Vector_3D& light_source) const;
 
-		RGB illuminate_diffused(const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
-			RGB dif = k_diff * ( normal_at(view_ray, t_of_intersect).normal() * light_vector_to_object( light, view_ray, t_of_intersect ).normal());
-			return dif * light.light_source_intensity;
-		}
+		RGB illuminate_diffused(const LightSource& light, const Ray& view_ray, double t_of_intersect) const;
 
-		RGB illuminate_specular(const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
-			double base = halfway_vector( light_vector_to_object( light, view_ray, t_of_intersect ), view_ray.direction()).normal() * normal_at(view_ray, t_of_intersect);
-			double same_rgb = k_specular * std::pow(base, n_specular);
-			same_rgb = same_rgb * light.light_source_intensity;
-			return RGB(same_rgb,same_rgb,same_rgb);
-		}
+		RGB illuminate_specular(const LightSource& light, const Ray& view_ray, double t_of_intersect) const;
 
-		RGB illuminate_ambient(const LightSource& light) const {
-			return k_ambient * light.ambient_intensity;
-		}
+		RGB illuminate_ambient(const LightSource& light) const;
 
 };
 
--- Projects/01/lib/domain/material.cpp
+++ Projects/01/lib/domain/material.cpp
@@ -7,5 +7,49 @@
 
 #include "raytracer/domain/material.h"
 
+RGB Material::illuminate(const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
+	if( in_shadow_at(light, view_ray, t_of_intersect) ) {
+		return illuminate_ambient(light);
+	} else {
+		return illuminate_diffused(light, view_ray, t_of_intersect) + illuminate_specular(light, view_ray, t_of_intersect) + illuminate_ambient(light);
+	}
+}
 
+Vector_3D Material::light_vector_to_object( const LightSource& light, const Ray& view_ray, double t_of_intersect ) const {
+	return light.light_source_location - view_ray.at(t_of_intersect);
+}
 
+bool Material::in_shadow_at( const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
+	return light_vector_to_object( light, view_ray, t_of_intersect ) * normal_at(view_ray, t_of_intersect) < 0;
+}
+
+#ifdef LOGGING
+std::string Material::stringify_object() const {
+	std::string info = "";
+	info += "k_diff: " + k_diff.to_string() + list_sep;
+	info += "k_ambient: " + k_ambient.to_string() + list_sep;
+	info += "k_specular: " + boost::lexical_cast<std::string>( k_specular ) + list_sep;
+	info += "n_specular: " + boost::lexical_cast<std::string>( n_specular ) + sep;
+	return info;
+}
+#endif
+
+Vector_3D Material::halfway_vector( const Vector_3D& viewpoint, const Vector_3D& light_source) const {
+	return (viewpoint + light_source) * 0.5;
+}
+
+RGB Material::illuminate_diffused(const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
+	RGB dif = k_diff * ( normal_at(view_ray, t_of_intersect).normal() * light_vector_to_object( light, view_ray, t_of_intersect ).normal());
+	return dif * light.light_source_intensity;
+}
+
+RGB Material::illuminate_specular(const LightSource& light, const Ray& view_ray, double t_of_intersect) const {
+	double base = halfway_vector( light_vector_to_object( light, view_ray, t_of_intersect ), view_ray.direction()).normal() * normal_at(view_ray, t_of_intersect);
+	double same_rgb = k_specular * std::pow(base, n_specular);
+	same_rgb = same_rgb * light.light_source_intensity;
+	return RGB(same_rgb,same_rgb,same_rgb);
+}
+
+RGB Material::illuminate_ambient(const LightSource& light) const {
+	return k_ambient * light.ambient_intensity;
+}
